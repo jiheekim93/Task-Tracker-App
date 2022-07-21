bdc472d5ab9af9c7c81b2be5f611a43f3505d4c5
Jihee Kim <jiheekim93@gmail.com>
2022-07-21 13:13:39 -0400

    fixed internal server issue

--- controllers/task.js
+++ controllers/task.js
@@ -1,6 +1,6 @@
 const express =  require('express')
 const router = express.Router()
-const Task = require('../models/task.js')
+const Tasks = require('../models/task.js')
 
 router.get('/', (req, res) => {
     Tasks.find({}, (err, foundTasks) => {
@@ -8,4 +8,22 @@ router.get('/', (req, res) => {
     })
 })
 
+router.post('/', (req, res) => {
+    Tasks.create(req.body, (err, createdTasks) => {
+        res.json(createdTasks)
+    })
+  })
+  
+  router.delete('/:id', (req, res)=>{
+    Tasks.findByIdAndRemove(req.params.id, (err, deletedTasks)=>{
+          res.json(deletedTasks);
+      });
+  });
+  
+  router.put('/:id', (req, res)=>{
+    Tasks.findByIdAndUpdate(req.params.id, req.body, {new:true}, (err, updatedTasks)=>{
+          res.json(updatedTasks);
+      });
+  });
+  
 module.exports = router
